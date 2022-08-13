316f7372234b678b7b8c0b70641e5c65886a1e69
SergKry <necroastral@mail.ru>
2022-08-13 18:14:23 +0400

    положительный тест

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class PostmanEchoTest {
+
+    @Test
+    void shouldPostLife() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("life: infinite")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("life: infinite"))
+        ;
+    }
+}
